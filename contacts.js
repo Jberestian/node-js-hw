a69696f8da97a5d17c10a394894968e894be264e
Yevhen <jberestian@gmail.com>
2022-10-17 18:41:54 +0300

    added changes

--- contacts.js
+++ contacts.js
@@ -1,27 +1,44 @@
-const contactsPath = require("./db/contacts.json");
+const contacts = require("./db/contacts");
 
-const fs = require("fs/promises");
-const path = require("path");
-
-const contactsFunct = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "getAll":
+      const allContacts = await contacts.getAll();
+      console.log(allContacts);
+      break;
+    default:
+      console.log("Unknown action");
+  }
 };
 
-function listContacts() {
-  // ...твій код
-}
+invokeAction({ action: "getAll" });
 
-function getContactById(contactId) {}
+// const fs = require("fs").promises;
+// const path = require("path");
 
-function removeContact(contactId) {
-  // ...твій код
-}
+// const contactsFunct = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+// };
 
-function addContact(name, email, phone) {
-  // ...твій код
-}
+// function listContacts() {
+//     readFile(contactsPath, 'utf-8')
+//         .then(data => console.log(JSON.parse(data))
+//             .catch(err => console.log(err));
+// }
 
-module.exports = contactsFunct;
+// function getContactById(contactId) {
+//   // ...твій код
+// }
+
+// function removeContact(contactId) {
+//   // ...твій код
+// }
+
+// function addContact(name, email, phone) {
+//   // ...твій код
+// }
+
+// module.exports = contactsFunct;
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-console.log(" hekko:");
+console.log(" hello:");
 
-// const fs = require("fs");
-// const path = require("path");
+const fs = require("fs/promises");
+const path = require("path");
 
-const contacts = require("./contacts");
+// const contactPath = `${__dirname}./db/contacts.json`;
+const contactPath = path.join(__dirname, "./db/contacts.json");
+
+const getAll = async () => {
+  const data = await fs.readFile(contactPath, "utf-8");
+  return JSON.parse(data);
+};
+
+module.exports = {
+  getAll,
+};
